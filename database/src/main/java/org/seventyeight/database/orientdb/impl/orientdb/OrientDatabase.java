89f74ca145ca76dc8e59d2ea4a59f1e5af60a936
Christian Wolfgang <wolfgarnet@gmail.com>
2012-11-21 14:48:46 +0100

    Working on indexes

--- database/src/main/java/org/seventyeight/database/orientdb/impl/orientdb/OrientDatabase.java
+++ database/src/main/java/org/seventyeight/database/orientdb/impl/orientdb/OrientDatabase.java
@@ -1,12 +1,19 @@
 package org.seventyeight.database.orientdb.impl.orientdb;
 
 import com.orientechnologies.orient.core.db.graph.OGraphDatabase;
+import com.orientechnologies.orient.core.index.OIndex;
+import com.orientechnologies.orient.core.index.OIndexDefinition;
+import com.orientechnologies.orient.core.index.OIndexDefinitionFactory;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import org.seventyeight.database.Database;
 import org.seventyeight.database.IndexValueType;
 import org.seventyeight.database.Query;
 import org.seventyeight.database.orientdb.impl.orientdb.query.OrientIndexBuilder;
 import org.seventyeight.database.orientdb.impl.orientdb.query.OrientPutIndexBuilder;
 import org.seventyeight.database.query.IndexType;
+
+import java.util.*;
 
 /**
  * User: cwolfgang
@@ -30,11 +37,33 @@ public class OrientDatabase implements Database<OGraphDatabase, OrientNode> {
         return new OrientNode( this );
     }
 
-    public void createIndex( OrientNode node, String index, IndexType type, IndexValueType valueType ) {
-        new OrientIndexBuilder( db, "letter" ).setIndexValueType( valueType ).setIndexType( type ).build().execute();
+    public void createIndex( String index, IndexType type, IndexValueType valueType ) {
+        //new OrientIndexBuilder( db, "letter" ).setIndexValueType( valueType ).setIndexType( type ).build().execute();
+
+        OIndexDefinition def = OIndexDefinitionFactory.createIndexDefinition( null, Collections.singletonList( "letter" ), Collections.singletonList( OType.FLOAT ) );
+        OIndex idx = db.getMetadata().getIndexManager().createIndex( index, type.toString(), null, null, null );
+        db.getMetadata().getIndexManager().create();
+
+
+        System.out.println( db.getMetadata().getIndexManager().getIndex( index ).getSize() );
+        System.out.println( Arrays.asList( db.getMetadata().getIndexManager().getIndex( index ).getKeyTypes() ) );
     }
 
     public <T> void putIndex( String name, OrientNode node, T key ) {
-        new OrientPutIndexBuilder( node, name ).setKey( key ).build().execute();
+        //new OrientPutIndexBuilder( node, name ).setKey( key ).build().execute();
+        db.getMetadata().getIndexManager().getIndex( name ).put( key, node.getDocument() );
+    }
+
+    public <T> List<OrientNode> getFromIndex( String name, T key ) {
+        Collection<ODocument> docs = db.getMetadata().getIndexManager().getIndex( name ).getEntries( Collections.singleton( key ) );
+
+
+        List<OrientNode> nodes = new LinkedList<OrientNode>();
+
+        for( ODocument d : docs ) {
+            nodes.add( new OrientNode( this, d ) );
+        }
+
+        return nodes;
     }
 }
--- database/src/test/java/org/seventyeight/database/orientdb/OrientIndexerTest.java
+++ database/src/test/java/org/seventyeight/database/orientdb/OrientIndexerTest.java
@@ -7,6 +7,12 @@ import org.seventyeight.database.IndexValueType;
 import org.seventyeight.database.Query;
 import org.seventyeight.database.orientdb.impl.orientdb.query.OrientIndexBuilder;
 import org.seventyeight.database.orientdb.impl.orientdb.OrientNode;
+import org.seventyeight.database.query.IndexType;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author cwolfgang
@@ -24,14 +30,27 @@ public class OrientIndexerTest {
 
     @Test
     public void test1() {
+        /*
         Query q = new OrientIndexBuilder( orule.getDB().getInternalDatabase(), "letter" ).setIndexValueType( IndexValueType.STRING ).build();
         q.execute();
         System.out.println( "q: " + q );
+        */
+
+        orule.getDB().createIndex( "letter", IndexType.REGULAR, IndexValueType.STRING );
 
         OrientNode n1 = orule.CreateNode( "letter", "a" );
         OrientNode n2 = orule.CreateNode( "letter", "b" );
         OrientNode n3 = orule.CreateNode( "letter", "c" );
 
         orule.getDB().putIndex( "letter", n1, "a" );
+        orule.getDB().putIndex( "letter", n2, "b" );
+        orule.getDB().putIndex( "letter", n3, "c" );
+
+        List<OrientNode> nodes = orule.getDB().getFromIndex( "letter", "b" );
+        assertThat( nodes.size(), is( 1 ) );
+
+        System.out.println( nodes );
     }
+
+
 }
